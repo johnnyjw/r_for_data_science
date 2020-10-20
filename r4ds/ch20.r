54e1009d5650551a665a63221a3b60802733e790
Johnny Wharton <johnnyjw@gmail.com>
2020-10-20 07:50:32 +0100

    p409 ex

--- r4ds/ch20.r
+++ r4ds/ch20.r
@@ -105,3 +105,74 @@ gapminder %>%
   semi_join(bad_fit, by = "country") %>% 
   ggplot(aes(year, lifeExp, color = country)) +
     geom_line()
+
+#Exs 409
+#1
+normalit<-function(m){
+  (m - mean(m))/(mean(m))
+}
+
+ny_gapminder <- gapminder %>% 
+  mutate(norm_year = normalit(year))
+
+ny_by_country <- ny_gapminder %>%
+  group_by(country, continent) %>% 
+  nest()
+
+#country model fitting function
+ny_country_model <- function(df) {
+  lm(lifeExp ~ I(poly(norm_year,3)), data = df)
+}
+
+#modelling
+ny_by_country <- ny_by_country %>%
+  mutate(model = map(data, ny_country_model))
+ny_by_country
+
+#all the models produced - now need to add residuals
+ny_by_country <- ny_by_country %>% 
+  mutate(
+    resids = map2(data, model, add_residuals)
+  )
+ny_by_country
+
+ny_glance <- ny_by_country %>% 
+  mutate(glance = map(model, broom::glance)) %>% 
+  unnest(glance, .drop = TRUE)
+ny_glance %>% 
+  arrange(r.squared)
+
+glance %>% 
+  arrange(r.squared)
+
+ny_glance_rw <- ny_by_country %>% 
+  filter(country=='Rwanda') %>% 
+  mutate(predict = map2(data, model, add_predictions)) %>% 
+  unnest(predict, .drop = TRUE)
+ny_glance_rw
+
+ny_glance_rw %>% 
+  ggplot(aes(year, lifeExp)) +
+  geom_point() + 
+  geom_smooth(aes(year, pred))
+
+# Much improved r2 with polynomial
+ny_glance %>% 
+  ggplot(aes(continent, r.squared)) +
+  geom_jitter(width = 0.5)
+
+ny_glance_rw %>% 
+  ggplot(aes(norm_year, lifeExp)) +
+  geom_point() + 
+  geom_smooth(aes(norm_year, pred))
+
+ny_glance_rw
+model_matrix(ny_glance_rw, lifeExp ~ I(poly(norm_year,3)))
+
+#get the coefficients
+regressionny <- ny_by_country %>% 
+  mutate(regressions = map(model, broom::tidy)) %>% 
+  unnest(regressions)
+
+regressionny %>% 
+  filter(country=='Rwanda')
